bb978c7356a46639024b5c6bfbc6a72f943da140
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-01-16 01:49:07 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = (props) => {
+    return (
+        <div>
+            <h1>Order Summery</h1>
+            <p>Selected Item: {props.cart.length}</p>
+
+        </div>
+    );
+};
+
+export default Cart;
+
